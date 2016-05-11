6594950cbfa2628e086de9966adfd91bb70afee4
HandsomeDuke <591957810@qq.com>
2016-05-11 12:51:42 +0800

    -m

---
+++ 11.4/hello.js
@@ -0,0 +1,19 @@
+var age = 90;
+if (age > 40) {
+    if (age >= 90) { alert("耄耋之年"); } else {
+        alert("四十不惑");
+    }
+} else {
+    alert("人生的路还很长");
+}
+
+for (var i = 0; i < 100; i++) {
+    console.log(i)
+        //在控制台输出信息
+}
+
+function sayHello(name) {
+    console.log(name + ", 你好");
+}
+
+sayHello("杨老师")
