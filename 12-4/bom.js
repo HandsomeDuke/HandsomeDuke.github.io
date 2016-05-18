6ca574a0a367f69c9ae9f7720caab7e4a7bfff1a
HandsomeDuke <591957810@qq.com>
2016-05-18 12:40:40 +0800

    -m

---
+++ 12-4/bom.js
@@ -0,0 +1,37 @@
+// 对象的声明及使用
+
+var person = {
+	name: 'Mr. right',
+	gender: 'male',
+	age: 20,
+	sing: function(){
+		console.log('正在唱歌');
+	}
+}
+
+console.log(person.name);
+person.sing();
+
+for (var i in console) {
+	console.log(i + ":" + console[i]);
+}
+console.clear();
+
+console.log(typeof person);
+
+//布尔类型
+
+console.log(typeof false)
+console.log(typeof 'false')
+
+//数字类型
+
+console.log(typeof 100);
+
+//字符串类型
+console.log(typeof person.name)
+
+//定义数组
+var sites = ('baidu.com', 'sohu.com', 'sina.com.cn', '163.com');
+console.log(sites[0]);
+console.log(typeof sites);
